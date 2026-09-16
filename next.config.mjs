43417bfc8ab3739752e50db112c2859e25168fcb
Opxio <hello@opxio.io>
2026-09-16 00:06:06 -0400

    Add clean /dashboard route

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/dashboard.html',
+      },
+    ]
+  },
   async headers() {
     return [
+      {
+        source: '/dashboard',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: 'frame-ancestors *' },
+          { key: 'Cache-Control', value: 'public, s-maxage=300, stale-while-revalidate=86400' },
+        ],
+      },
       {
         source: '/dashboard.html',
         headers: [
